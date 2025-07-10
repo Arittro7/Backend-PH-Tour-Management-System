e63f579186c02427c06b1d1d2481cea5955698ae
Arittro7 <arittrowpdev@gmail.com>
2025-07-10 23:34:07 +0600

    Global error handler

---
+++ src/app/modules/user/user.controllers.ts
@@ -0,0 +1,24 @@
+/* eslint-disable no-console */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { NextFunction, Request, Response, } from "express"
+import httpStatus from 'http-status-codes';
+import { userServices } from "./user.services";
+
+const createUser = async (req: Request, res: Response, next : NextFunction) =>{
+  try {
+    const user = await userServices.createUser(req.body)
+
+    res.status(httpStatus.CREATED).json({
+      message: "User created Successfully",
+      user
+    })
+  } catch (err : any) {
+    console.log(err);
+    next(err)
+  }
+}
+
+export const UserControllers = {
+  createUser
+}
