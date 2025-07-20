4c7e1159b332bb540882aa223eccc48077d9557d
Arittro7 <arittrowpdev@gmail.com>
2025-07-21 00:01:31 +0600

    Google Authentication

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -4,10 +4,14 @@ import { catchAsync } from "../../utils/catchAsync"
 import { sendResponse } from "../../utils/sendResponse"
 import httpStatus from 'http-status-codes';
 import { AuthServices } from "./auth.service";
+import AppError from "../../errorHelpers/AppError";
+import { setAuthCookie } from "../../utils/setCookie";
 
 const credentialsLogin = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
   
   const loginInfo = await AuthServices.credentialsLogin(req.body)
+  setAuthCookie(res, loginInfo) 
+  // note: setAuthCookie function will handle both access and refresh tokens functionality based on
 
   sendResponse(res, {
     success: true,
@@ -17,19 +21,56 @@ const credentialsLogin = catchAsync(async(req: Request, res: Response, next: Nex
   })
 })
 
-
 const getNewAccessToken = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
-  
-  const tokenInfo = await AuthServices.credentialsLogin(req.body)
+  const refreshToken = req.cookies.refreshToken
+  if(!refreshToken){
+    throw new AppError(httpStatus.BAD_REQUEST, "No Refresh Token received from cookies")
+  }
+  const tokenInfo = await AuthServices.getNewAccessToken(refreshToken)
+  setAuthCookie(res, tokenInfo)
 
   sendResponse(res, {
     success: true,
     statusCode : httpStatus.OK,
-    message: "User Logged In Successfully",
+    message: "New Access Token Retrieve Successfully",
     data: tokenInfo 
   })
 })
 
+const logout = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
+  res.clearCookie("accessToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax"
+  })
+  res.clearCookie("refreshToken", {
+    httpOnly: true,
+    secure: false,
+    sameSite: "lax"
+  })
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Logout Successfully",
+    data: null
+  })
+})
+
+const resetPassword = catchAsync(async(req: Request, res: Response, next: NextFunction) => {
+  const oldPassword = req.body.oldPassword
+  const newPassword = req.body.newPassword
+  const decodedToken = req.user
+  await AuthServices.resetPassword(oldPassword, newPassword, decodedToken)
+
+  sendResponse(res, {
+    success: true,
+    statusCode: httpStatus.OK,
+    message: "Password Changed Successfully",
+    data: null
+  })
+})
+
 export const AuthController = {
-  credentialsLogin
+  credentialsLogin, getNewAccessToken, logout, resetPassword
 }
