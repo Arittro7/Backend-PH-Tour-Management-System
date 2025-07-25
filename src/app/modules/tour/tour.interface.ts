560a1758dd043e47e434ffa61ecf40a61de6ffb4
Arittro7 <arittrowpdev@gmail.com>
2025-07-25 16:24:50 +0600

    part 4 done

---
+++ src/app/modules/tour/tour.interface.ts
@@ -0,0 +1,24 @@
+import { Types } from "mongoose";
+
+export interface ITourTypes{
+  name: string
+}
+
+export interface ITour{
+  title : string;
+  slug : string;
+  description ?: string;
+  images ?: string[];
+  location ?: string;
+  costFrom ?: number;
+  startDate ?: Date; //`JS date use as type 
+  endDate ?: Date;
+  included ?: string[]; //` [] bcoz multiple items can be added 
+  excluded ?: string[];
+  amenities ?: string[];
+  tourPlan ?: string[];
+  maxGuest ?: number;
+  minAge ?: number;
+  division : Types.ObjectId
+  tourType : Types.ObjectId //> Filter will applied based on tour type(short, long, week etc). Another file will created to contain tour types.
+}
---
+++ src/app/modules/tour/tour.model.ts
@@ -0,0 +1,51 @@
+import { model, Schema} from "mongoose";
+import { ITour, ITourTypes } from "./tour.interface";
+
+const tourTypesSchema = new Schema<ITourTypes>({
+  name: {type: String, required:true, unique:true}
+},{
+  timestamps: true
+})
+
+export const TourTypes = model<ITourTypes>("TourType", tourTypesSchema)
+
+const tourSchema = new Schema<ITour>(
+  {
+    title: { type: String, required: true },
+    slug: { type: String, required: true, unique: true },
+    description: {type: String},
+    images:{type:[String], default:[]},
+    location:{type:String},
+    costFrom:{type:Number},
+    startDate:{type:Date}, //date from JS types
+    endDate:{type:Date},
+    included:{type:[String], default:[]},
+    excluded:{type:[String], default:[]},
+    amenities:{type:[String], default:[]},
+    tourPlan:{type:[String], default:[]},
+    maxGuest:{type:Number},
+    minAge:{type:Number},
+    division:{ // this is a connection with division model 
+      type: Schema.Types.ObjectId,
+      ref: "Division", //imp: Make sure to use the model name which is Division. As the variable also named Division. Tips: name inside quotation
+      required: true
+    },
+    tourType:{
+      type: Schema.Types.ObjectId,
+      ref:"TourTypes",
+      required: true
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Tour = model<ITour>("Tour", tourSchema)
+
+
+//>----------------------------------------<//
+
+/**
+ * `Division and Tour type are two different collection. To link with other collection 1. ObjectId of that collection is req. thats why Schema.Types used from mongoose 2. Referencing with that collection using ref: "ModelName" (make sure model is created like division.model.ts)   
+ */
